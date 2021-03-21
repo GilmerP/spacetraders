d4a840d3c8a3258ab1456de2fd067434e43b5947
gilli <p.gilmer@gmx.de>
2021-03-21 01:21:46 +0100

    added canvas and static props to map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,15 +1,43 @@
 <template>
-  <div></div>
+  <div class="container">
+    <canvas id="canvas" width="1000" height="1000"> </canvas>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 
 export default defineComponent({
   setup() {
-    return {};
+    const fieldCount = ref(100);
+    onMounted(() => {
+      const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+      const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+      ctx.fillStyle = "#969696";
+      ctx.beginPath();
+      ctx.arc(20 * 5 + 500, 500 - -25 * 5, 50, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.beginPath();
+      ctx.arc(16 * 5 + 500, 500 - 17 * 5, 50, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.beginPath();
+      ctx.arc(-48 * 5 + 500, 500 - -7 * 5, 50, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.beginPath();
+      ctx.arc(-75 * 5 + 500, 500 - 82 * 5, 50, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.beginPath();
+      ctx.arc(-23 * 5 + 500, 500 - -28 * 5, 20, 0, 2 * Math.PI);
+      ctx.fill();
+    });
+    return { fieldCount };
   }
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+canvas {
+  box-sizing: border-box;
+  background: black;
+}
+</style>

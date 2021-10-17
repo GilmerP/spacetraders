f8327dd1f84e010e52153fb575fdb2bf30bada44
Pascal <p.gilmer@gmx.de>
2021-10-17 23:26:37 +0200

    WIP: rework map

---
+++ src/ts/SvgHelper.ts
@@ -0,0 +1,36 @@
+export default class SvgHelper {
+  /**
+   * draws a line between the given coordinates
+   */
+  static drawLine(x1: number, y1: number, x2: number, y2: number, id = "") {
+    let newLine = (document.getElementById("travelPath") as Element) as SVGLineElement;
+    if (!newLine) newLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    newLine.setAttribute("id", id);
+    newLine.setAttribute("x1", String(x1));
+    newLine.setAttribute("y1", String(y1));
+    newLine.setAttribute("x2", String(x2));
+    newLine.setAttribute("y2", String(y2));
+    return newLine;
+  }
+
+  /**
+   * returns the DOMRect Object from the HTMLElement with the given id
+   * @param id The id of the DOM Object to retrieve the DOMRect from
+   */
+  static getBoxFromSvg(id: string): DOMRect {
+    if (!id) throw new Error("Parameter must be a non-empty string");
+    const element = document.getElementById(id) as Element;
+    if (!(element instanceof SVGElement)) throw new Error("Element must be of type SVG");
+    return (element as SVGSVGElement).getBBox();
+  }
+
+  /**
+   * returns the center of the given box as coordinates
+   */
+  static getCenterOfBox(box: DOMRect) {
+    return {
+      x: box.x + box.width / 2,
+      y: box.y + box.height / 2
+    };
+  }
+}

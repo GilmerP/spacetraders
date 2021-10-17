b65a33d99ba4cf560f538a7e766d7ee6d0e45a69
Pascal <p.gilmer@gmx.de>
2021-10-17 16:34:58 +0200

    removed components that are unnecessary for now

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory, Router } from "vue-router";
 import Home from "../views/Home.vue";
 import Ships from "../views/Ships.vue";
 import Trade from "../views/Trade.vue";
-import Login from "../views/Login.vue";
-import Signup from "../views/Signup.vue";
 import Map from "../views/Map.vue";
 
 const routes = [
@@ -11,16 +9,6 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login
-  },
-  {
-    path: "/signup",
-    name: "Signup",
-    component: Signup
   },
   {
     path: "/ships",
